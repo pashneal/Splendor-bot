fe304d964dfd9025dbb5aed93916777f74b8e1a3
Neal Powell <pashneal@gmail.com>
2024-09-10 14:39:07 -0400

    feat: stacks of gems

--- scaffolding/web/src/routes/demo/+page.svelte
+++ scaffolding/web/src/routes/demo/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import Card from "$lib/components/Card.svelte";
-  import CardDetailGem from "$lib/components/CardDetailGem.svelte";
-  import CardDisplay from "$lib/components/CardDisplay.svelte";
+  import CardDislay from "$lib/components/CardDisplay.svelte";
+  import GemToken from "$lib/components/GemToken.svelte";
+  import Bank from "$lib/components/Bank.svelte";
 </script>
 
 <svelte:head>
@@ -9,50 +9,36 @@
 	<meta name="description" content="A demonstration of the the stourney app running a splendor game" />
 </svelte:head>
 
-
-<div class="cards">
-  <CardDisplay>
-    <Card card_name={"emerald"} points={5}>
-      <CardDetailGem gem_name={"emerald"} number={7}/>
-      <CardDetailGem gem_name={"ruby"} number={5}/>
-      <CardDetailGem gem_name={"diamond"} number={6}/>
-    </Card>
-    <Card card_name={"ruby"} points={7}>
-      <CardDetailGem gem_name={"ruby"} number={2}/>
-      <CardDetailGem gem_name={"sapphire"} number={3}/>
-      <CardDetailGem gem_name={"diamond"} number={1}/>
-    </Card>
-    <Card card_name={"sapphire"} points={6}>
-      <CardDetailGem gem_name={"ruby"} number={2}/>
-      <CardDetailGem gem_name={"sapphire"} number={3}/>
-      <CardDetailGem gem_name={"diamond"} number={1}/>
-    </Card>
-    <Card card_name={"onyx"} points={4}/>
-  </CardDisplay>
-
-  <CardDisplay>
-    <Card card_name={"onyx"} points={2}/>
-    <Card card_name={"diamond"} points={2}/>
-    <Card card_name={"emerald"} points={3}/>
-    <Card card_name={"sapphire"} points={2}/>
-  </CardDisplay>
-
-  <CardDisplay>
-    <Card card_name={"ruby"} points={2}/>
-    <Card card_name={"emerald"} points={0}/>
-    <Card card_name={"diamond"} points={1}/>
-    <Card card_name={"onyx"}/>
-  </CardDisplay>
+<div class="game">
+  <Bank>
+    <GemToken tokenName={"gold"} numRemaining={3} />
+    <GemToken tokenName={"emerald"} numRemaining={6} />
+    <GemToken tokenName={"diamond"} numRemaining={2} />
+    <GemToken tokenName={"onyx"} numRemaining={5} />
+    <GemToken tokenName={"ruby"} numRemaining={1} />
+    <GemToken tokenName={"sapphire"} numRemaining={2} />
+  </Bank>
+  <div>
+    <CardDislay/>
+  </div>
 </div>
 
 <style>
-  .cards {
-    max-width: 800px;
-    max-height: 600px;
-    margin-inline: auto;
-    left: 0;
-    right: 0;
+  .game {
+    -khtml-user-select: none;
+    -o-user-select: none;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 80%;
+    height: 80%;
+    gap: 5%;
   }
+
 
 
 </style>
