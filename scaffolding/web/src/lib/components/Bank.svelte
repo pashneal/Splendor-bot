6b531d9bbcb697f2cba1e43af00f90dc7f238993
Neal Powell <pashneal@gmail.com>
2024-09-10 14:39:33 -0400

    fix: make sure all latest components are available

---
+++ scaffolding/web/src/lib/components/Bank.svelte
@@ -0,0 +1,14 @@
+<div class="gems">
+  <slot></slot>
+</div>
+
+
+<style>
+  .gems {
+    width: 10%;
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+  }
+</style>
---
+++ scaffolding/web/src/lib/components/CardDisplay.svelte
@@ -0,0 +1,52 @@
+<script>
+  import Card from "$lib/components/Card.svelte";
+  import CardDetailGem from "$lib/components/CardDetailGem.svelte";
+  import CardRow from "$lib/components/CardRow.svelte";
+</script>
+
+<div class="cards">
+  <CardRow>
+    <Card card_name={"emerald"} points={5}>
+      <CardDetailGem gem_name={"emerald"} number={7}/>
+      <CardDetailGem gem_name={"ruby"} number={5}/>
+      <CardDetailGem gem_name={"diamond"} number={6}/>
+    </Card>
+    <Card card_name={"ruby"} points={7}>
+      <CardDetailGem gem_name={"ruby"} number={2}/>
+      <CardDetailGem gem_name={"sapphire"} number={3}/>
+      <CardDetailGem gem_name={"diamond"} number={1}/>
+    </Card>
+    <Card card_name={"sapphire"} points={6}>
+      <CardDetailGem gem_name={"ruby"} number={2}/>
+      <CardDetailGem gem_name={"sapphire"} number={3}/>
+      <CardDetailGem gem_name={"diamond"} number={1}/>
+    </Card>
+    <Card card_name={"onyx"} points={4}/>
+  </CardRow>
+
+  <CardRow>
+    <Card card_name={"onyx"} points={2}/>
+    <Card card_name={"diamond"} points={2}/>
+    <Card card_name={"emerald"} points={3}/>
+    <Card card_name={"sapphire"} points={2}/>
+  </CardRow>
+
+  <CardRow>
+    <Card card_name={"ruby"} points={2}/>
+    <Card card_name={"emerald"} points={0}/>
+    <Card card_name={"diamond"} points={1}/>
+    <Card card_name={"onyx"}/>
+  </CardRow>
+</div>
+
+<style>
+  .cards {
+    max-width: 800px;
+    max-height: 600px;
+    margin-inline: auto;
+    left: 0;
+    right: 0;
+  }
+
+
+</style>
