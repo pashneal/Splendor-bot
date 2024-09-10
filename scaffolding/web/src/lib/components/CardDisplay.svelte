ba7743dbcbe9c22cf40305daffae7e2349a71e48
Neal Powell <pashneal@gmail.com>
2024-09-10 16:03:53 -0400

    feat: basic card stack

--- scaffolding/web/src/lib/components/CardDisplay.svelte
+++ scaffolding/web/src/lib/components/CardDisplay.svelte
@@ -2,6 +2,7 @@
   import Card from "$lib/components/Card.svelte";
   import CardDetailGem from "$lib/components/CardDetailGem.svelte";
   import CardRow from "$lib/components/CardRow.svelte";
+  import CardStack from "./CardStack.svelte"; 
 </script>
 
 <div class="cards">
@@ -22,6 +23,7 @@
       <CardDetailGem gem_name={"diamond"} number={1}/>
     </Card>
     <Card card_name={"onyx"} points={4}/>
+    <CardStack count={31}/>
   </CardRow>
 
   <CardRow>
@@ -29,6 +31,7 @@
     <Card card_name={"diamond"} points={2}/>
     <Card card_name={"emerald"} points={3}/>
     <Card card_name={"sapphire"} points={2}/>
+    <CardStack count={31}/>
   </CardRow>
 
   <CardRow>
@@ -36,6 +39,7 @@
     <Card card_name={"emerald"} points={0}/>
     <Card card_name={"diamond"} points={1}/>
     <Card card_name={"onyx"}/>
+    <CardStack count={31}/>
   </CardRow>
 </div>
 
