6ead64e82d37de6f3f7220bd5db60770e08fe9da
Neal Powell <pashneal@gmail.com>
2024-09-11 20:56:35 -0400

    fix: add all extra files needed to make this work

---
+++ scaffolding/web/src/lib/components/Avatar.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import av1 from "$lib/assets/av1.svg";
+  import av2 from "$lib/assets/av2.svg";
+  import av3 from "$lib/assets/av3.svg";
+  import av4 from "$lib/assets/av4.svg";
+  import av5 from "$lib/assets/av5.svg";
+  import av6 from "$lib/assets/av6.svg";
+  import av7 from "$lib/assets/av7.svg";
+  import av8 from "$lib/assets/av8.svg";
+  import av9 from "$lib/assets/av9.svg";
+
+  export let avatar : number = 0;
+  export let name : string = "Player " + avatar;
+  let avatars = [av1, av2, av3, av4, av5, av6, av7, av8, av9];
+</script>
+
+<div>
+  <img src={avatars[avatar]} alt="avatar" draggable="false"/>
+  <p class="name">{name}</p>
+</div>
+
+<style>
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 30px;
+    width: 100px;
+    height: 100px;
+  }
+
+  .name {
+    border-bottom: 1px dashed black;
+    font-family: 'Quicksand Book';
+    font-weight: bold;
+  }
+  img {
+    width : 100%;
+    height: 100%;
+  }
+
+  div:hover{
+    background-color: #f0f0f0;
+    border-radius: 10px;
+  }
+</style>
+
---
+++ scaffolding/web/src/lib/components/Player.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import Avatar from "./Avatar.svelte"; 
+  import Bank from "./Bank.svelte";
+  import GemTokenSmall from "./GemTokenSmall.svelte";
+
+  export let name;
+  export let avatar: number = 0;
+</script>
+
+<div class="player-container">
+  <Avatar avatar={avatar} name={name} />
+  <div class="gems-container">
+      <GemTokenSmall tokenName={"gold"} numRemaining={3} />
+      <GemTokenSmall tokenName={"emerald"} numRemaining={6} />
+      <GemTokenSmall tokenName={"diamond"} numRemaining={2} />
+      <GemTokenSmall tokenName={"onyx"} numRemaining={5} />
+      <GemTokenSmall tokenName={"ruby"} numRemaining={1} />
+      <GemTokenSmall tokenName={"sapphire"} numRemaining={2} />
+  </div>
+</div>
+
+<style>
+  .player-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 25%;
+    height: 20%;
+  }
+  .gems-container {
+    width: 200px;
+    flex-direction: row;
+    flex-wrap: wrap;
+    display: flex;
+    height: 100%;
+    gap: 2%;
+  }
+
+</style>
+
